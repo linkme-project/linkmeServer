269244a564b783e6dc581216fbf238ec25eaece8
Keunhak Lim <limkeunhak@gmail.com>
2019-06-08 00:24:47 +0900

    add healthcheck

--- src/main/java/com/nakkunakku/linkme/healthy/HealthCheckController.java
+++ src/main/java/com/nakkunakku/linkme/healthy/HealthCheckController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/healthy")
+@RequestMapping("/")
 public class HealthCheckController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String healthy() {
+        return "OK";
+    }
+    
+    @RequestMapping(value = "/healthy", method = RequestMethod.GET)
     public String healthCheck() {
         return "OK";
     }
