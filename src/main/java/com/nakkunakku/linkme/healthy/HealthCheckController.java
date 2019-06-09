fe0eb8ec50b329c7d6607ae64974349094a2ba87
Keunhak Lim <limkeunhak@gmail.com>
2019-06-09 19:08:16 +0900

    change health check

--- src/main/java/com/nakkunakku/linkme/healthy/HealthCheckController.java
+++ src/main/java/com/nakkunakku/linkme/healthy/HealthCheckController.java
@@ -5,15 +5,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/healthy")
 public class HealthCheckController {
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String healthy() {
-        return "OK";
-    }
-    
-    @RequestMapping(value = "/healthy", method = RequestMethod.GET)
     public String healthCheck() {
         return "OK";
     }
